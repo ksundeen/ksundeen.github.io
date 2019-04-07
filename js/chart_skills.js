f9a373deff8dfe22728650a2fe95616474a43321
ksundeen <kim.h.sundeen@gmail.com>
2019-04-07 14:31:44 -0500

    Updated skills levels

--- js/chart_skills.js
+++ js/chart_skills.js
@@ -1,8 +1,8 @@
 var devData = {
-    labels: ["Python APIs", "Git/Subversion", "Bootstrap", "JavaScript", "jQuery", "Azure/AWS", "Angular/Typescript", "UX", "Node.js", ".NET C#"],
+    labels: ["Python APIs", "Source Control", "Bootstrap", "JavaScript", "Angular/Typescript", "jQuery", "VM Architecture-Azure/AWS", "UX", "Node.js", ".NET C#"],
     datasets: [{
         label: 'Dev',
-        data: [9,8,7,7,6,6,5,5,4,4],
+        data: [9,8,8,8,7,7,6,6,6,6],
         backgroundColor: 'rgba(66, 134, 244, 0.5)',
         borderColor: "rgb(66, 134, 244)",
         borderWidth: 1,
@@ -13,7 +13,7 @@ var geoData = {
     labels: ["ArcPy Python", "Esri JavaScript API 3.x", "Leaflet API", "D3", ".NET GIS Customization", "Google Maps API", "GDAL", "Esri JavaScript API 4.x", "Spatial R", "Mapbox API"],
     datasets: [{
         label: 'Geo Dev',
-        data: [10,7,5,5,5,4,4,4,4,3],
+        data: [10,8,6,6,6,5,5,5,5,4],
         backgroundColor: 'rgba(5, 130, 15, 0.5)',
         borderColor: "rgb(5, 130, 15)",
         borderWidth: 1,
@@ -21,10 +21,10 @@ var geoData = {
 }
 
 var dbData = {
-    labels: ["MS Excel Customization", "ArcGIS Server", "ArcSDE/PostGIS", "SQL/PLSQL", "MySQL", "PostgreSQL", "Oracle", "R Regression"],
+    labels: ["MS Excel Customization", "ArcGIS Server", "ArcSDE/PostGIS", "PostgreSQL", "SQL/PLSQL", "SQL Server/IIS/WAMP/LAMP", "Oracle", "R Regression"],
     datasets: [{
         label: 'Data Access/Analytics',
-        data: [10,9,8,8,8,7,7,6],
+        data: [10,10,9,8,7,7,7,6],
         backgroundColor: 'rgba(198, 131, 13, 0.5)',
         borderColor: "rgb(198, 131, 13)",
         borderWidth: 1,
@@ -32,7 +32,7 @@ var dbData = {
 }
 
 var gisAppData = {
-    labels: ["ArcGIS Desktop", "ArcGIS Online", "Arc Pro", "Esri Web AppBuilder Dev", "ArcGIS Collector", "Survey123", "ArcGIS Enterprise", "QGIS"],
+    labels: ["ArcGIS Desktop", "ArcGIS Online", "ArcGIS Enterprise", "Esri Web AppBuilder Dev", "ArcGIS Collector", "QGIS", "Survey123", "Arc Pro"],
     datasets: [{
         label: 'GIS Applications',
         data: [10,10,9,9,9,8,8,7],
