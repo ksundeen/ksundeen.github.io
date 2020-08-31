e1da80c22e6e4cae1fc8324ef49316bb8f5da610
Kim Sundeen <kim.h.sundeen@gmail.com>
2020-08-30 20:09:47 -0500

    Updated skills and text description

--- js/chart_skills.js
+++ js/chart_skills.js
@@ -1,8 +1,8 @@
 var devData = {
-    labels: ["Python APIs", "Source Control", "Bootstrap", "JavaScript", "Angular/Typescript", "jQuery", "VM DevOps-Azure/AWS", "UX", "Node.js", ".NET C#"],
+    labels: ["React", "Python APIs", "JavaScript", "Source Control", "UI/UX Frameworks", "Angular/Typescript", "jQuery", ".NET C#", "IIS", "VM DevOps-Azure/AWS", "Node.js", ],
     datasets: [{
         label: 'Dev',
-        data: [9,8,8,8,7,7,6,6,6,6],
+        data: [9,8,8,8,8,7,7,6,6,6,6],
         backgroundColor: 'rgba(66, 134, 244, 0.5)',
         borderColor: "rgb(66, 134, 244)",
         borderWidth: 1,
@@ -10,10 +10,10 @@ var devData = {
 }
 
 var geoData = {
-    labels: ["ArcPy Python", "Esri JavaScript API 3.x", "Leaflet API", "D3", ".NET GIS Customization", "Google Maps API", "GDAL", "Esri JavaScript API 4.x", "Spatial R", "Mapbox API"],
+    labels: ["Open Layers", "ArcPy Python", "Esri JavaScript API 3.x", "Leaflet API", "D3", "Google Maps API", ".NET GIS", "GDAL", "Esri JavaScript API 4.x", "Mapbox API", "Spatial R"],
     datasets: [{
         label: 'Geo Dev',
-        data: [10,8,6,6,6,5,5,5,5,4],
+        data: [9,8,7,6,6,6,5,5,5,5,4],
         backgroundColor: 'rgba(5, 130, 15, 0.5)',
         borderColor: "rgb(5, 130, 15)",
         borderWidth: 1,
@@ -21,10 +21,10 @@ var geoData = {
 }
 
 var dbData = {
-    labels: ["MS Excel Customization", "ArcGIS Server", "ArcSDE/PostGIS", "PostgreSQL", "SQL/PLSQL", "Web Servers", "Oracle", "R Regression"],
+    labels: ["GeoServer", "ArcGIS Server", "PostgreSQL", "ArcSDE/PostGIS", "SQL/PLSQL", "Web Servers", "Oracle", "R Regression"],
     datasets: [{
-        label: 'Data Access/Analytics',
-        data: [10,10,9,8,7,7,7,6],
+        label: 'Map Servers/Analytics',
+        data: [8,8,8,8,7,7,7,6],
         backgroundColor: 'rgba(198, 131, 13, 0.5)',
         borderColor: "rgb(198, 131, 13)",
         borderWidth: 1,
@@ -32,10 +32,10 @@ var dbData = {
 }
 
 var gisAppData = {
-    labels: ["ArcGIS Desktop", "ArcGIS Online", "ArcGIS Enterprise", "Esri Web AppBuilder Dev", "ArcGIS Collector", "QGIS", "Survey123", "Arc Pro"],
+    labels: ["ArcGIS Desktop", "QGIS", "ArcGIS Pro", "ArcGIS Online", "ArcGIS Enterprise", "Esri Web AppBuilder Dev", "ArcGIS Collector", "Survey123"],
     datasets: [{
         label: 'GIS Applications',
-        data: [10,10,9,9,9,8,8,7],
+        data: [10,9,8,8,8,8,8,8,8],
         backgroundColor: 'rgba(178, 30, 14, 0.5)',
         borderColor: "rgb(178, 30, 14)",
         borderWidth: 1,
